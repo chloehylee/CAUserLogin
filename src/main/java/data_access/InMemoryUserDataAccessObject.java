4d574281c9146b2af40deb15fdd99e3e9d4a8fad
chloehylee <335732582@gapps.yrdsb.ca>
2024-10-28 03:36:49 -0400

    Task 2.1

--- src/main/java/data_access/InMemoryUserDataAccessObject.java
+++ src/main/java/data_access/InMemoryUserDataAccessObject.java
@@ -35,6 +35,16 @@ public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterfa
         return users.get(username);
     }
 
+    /**
+     * Set current user.
+     *
+     * @param name the name
+     */
+    @Override
+    public void setCurrentUser(String name) {
+        this.currentUser = name;
+    }
+
     @Override
     public void changePassword(User user) {
         // Replace the old entry with the new password
--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set current user.
+     * @param name the name
+     */
+    void setCurrentUser(String name);
 }
